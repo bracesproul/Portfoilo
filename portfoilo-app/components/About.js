7349d7bb67769c8dbb8bba42eb5d29c9e8839d88
Brace Sproul <braceasproul@gmail.com>
2022-03-12 07:02:03 +0000

    removed <a />, replaced with <Link />

--- portfoilo-app/components/About.js
+++ portfoilo-app/components/About.js
@@ -1,4 +1,5 @@
 import styles from '/styles/Home.module.css';
+import Link from 'next/link';
 
 export const About = () => {
     return (
@@ -7,9 +8,9 @@ export const About = () => {
         <div className={styles.aboutContainer}>
             <p className={styles.aboutText}>Hey!</p>
             <p className={styles.aboutText}>I&apos;m a 17 year old software engineer from the Bay Area passionate about building web applications and learning new skills. Right now I&apos;m very interested in Solidity and smart contracts. I have some experience building web application (mostly in Next.js/React) and I&apos;m looking to expand my knowledge and learn new technologies.</p>
-            <p className={styles.aboutText}>I&apos;m currently enrolled at Berkeley High School, completing my senior year. After graduating I plan on taking a gap year to further my knowledge and skills. If you&apos;re interested in collaborating for hiring me please feel free to <a className={styles.linkV} href="/contact">reach out</a>.</p>
+            <p className={styles.aboutText}>I&apos;m currently enrolled at Berkeley High School, completing my senior year. After graduating I plan on taking a gap year to further my knowledge and skills. If you&apos;re interested in collaborating for hiring me please feel free to <Link className={styles.linkV} href="/contact">reach out</Link>.</p>
             <p className={styles.aboutText}>Please feel free to check out my projects below, the source code and all information relating to them.</p>
-            <p className={styles.aboutText}>If you would like to contact me please click <a className={styles.linkV} href="/contact">here</a>.</p>
+            <p className={styles.aboutText}>If you would like to contact me please click <Link className={styles.linkV} href="/contact">here</Link>.</p>
         </div>
         </>
     )
